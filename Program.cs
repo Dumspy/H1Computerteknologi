36e8c1fb5a52152b26d265558a319fdbb34a1850
Felix Berger <felix.enok.berger@gmail.com>
2023-05-31 10:09:01 +0200

    readded everything

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using UserFolder;
+
+var userFolder = new Controller();
+Console.WriteLine(userFolder.GetPath());
+
+int number = 0;
+
+while (true)
+{
+    // Increase the number by one
+    number++;
+
+    // Print the current number
+    Console.WriteLine(number);
+
+    // Wait for one second
+    Thread.Sleep(1000);
+}
---
+++ UserFolder/Controller.cs
@@ -0,0 +1,41 @@
+﻿namespace UserFolder;
+
+public class Controller
+{
+    private readonly string _path;
+
+    public Controller()
+    {
+        _path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+    }
+    
+    public Controller(string path)
+    {
+        _path = path;
+    }
+
+    public string GetPath()
+    {
+        return _path;
+    }
+
+    public void CreateFile(string fileName)
+    {
+        string filePath = Path.Combine(_path, fileName, ".txt");
+
+        if (File.Exists(filePath))
+        {
+            Console.WriteLine("File already exists.");
+            return;
+        }
+
+        try
+        {
+            using StreamWriter writer = File.CreateText(filePath);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
